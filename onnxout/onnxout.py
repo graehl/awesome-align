53921e63b801d5cce75dc0e0a0f0c6adba2e573a
Jonathan Graehl <graehl@gmail.com>
2025-04-07 11:41:17 -0700

    onnxout.py modify onnx output

---
+++ onnxout/onnxout.py
@@ -0,0 +1,47 @@
+import onnx
+
+import typer
+from typing_extensions import Annotated
+
+import sys
+
+def log(x):
+   sys.stderr.write('# ' + str(x) + '\n')
+
+onnxoutapp = typer.Typer(no_args_is_help=True, add_completion=False)
+
+def onnx_inputs(path, inputs=None):
+  if inputs is None:
+    return [x.name for x in onnx.load(path).graph.input]
+  return inputs
+
+def modify_onnx_outputs(path, onnxpathout, outputs, inputs=None, checker=True):
+  onnx.utils.extract_model(path, onnxpathout, onnx_inputs(path, inputs), outputs)
+  if checker:
+    onnx.checker.check_model(onnxpathout)
+  return onnxpathout
+
+
+@onnxoutapp.command()
+def main(in_model: Annotated[str, typer.Argument(help="onnx input model")],
+         name: Annotated[str, typer.Argument(help="tensor name to outputs")] = "",
+         out_model: Annotated[str, typer.Argument(help="onnx output model (if empty, same as in_model)")] = "",
+         list_tensors: bool = typer.Option(False, "--list", "-l", help="list tensors (do not write out_model)"),
+):
+    log("loading %s" % in_model)
+    if list_tensors:
+        model = load_onnx_model(in_model)
+        graph = model.graph
+        for name in enumerate_model_node_outputs(model):
+            print(name)
+        log(graph.value_info)
+        for value_info_proto in graph.value_info:
+            log(value_info_proto.name)
+        return
+    if not len(name):
+        log("specify tensorname [onnx-outfile] or -l")
+        return False
+    modify_onnx_outputs(in_model, out_model, [name], inputs=None, checker=True)
+
+if __name__ == "__main__":
+    onnxoutapp()
